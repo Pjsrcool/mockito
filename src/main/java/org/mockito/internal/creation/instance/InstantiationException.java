3bcc4ff8f3222339aabb31531838963e8a003df2
nimakarimipour <karimipour.nima@gmail.com>
2021-04-13 04:01:12 +0000

    update

--- src/main/java/org/mockito/internal/creation/instance/InstantiationException.java
+++ src/main/java/org/mockito/internal/creation/instance/InstantiationException.java
@@ -4,14 +4,19 @@
  */
 package org.mockito.internal.creation.instance;
 
-import javax.annotation.Nullable;
-
 import org.mockito.exceptions.base.MockitoException;
 
+/**
+ * @deprecated since 2.15.4 because this internal class was leaking from the public API.
+ * For information why deprecated, see {@link org.mockito.plugins.InstantiatorProvider2}.
+ * Use {@link org.mockito.creation.instance.Instantiator} and {@link org.mockito.creation.instance.InstantiationException} types instead.
+ * <p>
+ * Exception generated when {@link Instantiator#newInstance(Class)} failed.
+ */
 @Deprecated
 public class InstantiationException extends MockitoException {
 
-    public InstantiationException(@Nullable String message, Throwable cause) {
+    public InstantiationException(String message, Throwable cause) {
         super(message, cause);
     }
 }
